aba822191d1f3b1be06be977faa89e42356cd331
yoyeh <yoyeh@princeton.edu>
2015-03-26 15:29:04 -0400

    Added View Controller Swift files for Now View and Contacts View.

---
+++ Refresh/Refresh/ContactsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ContactsViewController.swift
+//  Refresh
+//
+//  Created by Yolanda Yeh on 3/26/15.
+//  Copyright (c) 2015 Refresh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+}
