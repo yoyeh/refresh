b15b4bfbac3d528fca14e0a138ea90812e81dc20
yoyeh <yoyeh@princeton.edu>
2015-03-25 22:19:33 -0400

    Basic status page implementation: Available and Not Available buttons cause change in displayed label value.

--- Refresh/Refresh/ViewController.swift
+++ Refresh/Refresh/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var currentState: UILabel!
+    @IBAction func notavailableButton(sender: AnyObject) {
+        currentState.text = sender.currentTitle!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func availableButton(sender: UIButton) {
+        currentState.text = sender.currentTitle!
     }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
