2977d5272ea6bb54e18f6e15c869422e7b3d3570
yoyeh <yoyeh@princeton.edu>
2015-04-20 01:49:22 -0400

    Implement phone number input and subtleties.

--- Refresh/Refresh/PhoneVerificationViewController.swift
+++ Refresh/Refresh/PhoneVerificationViewController.swift
@@ -12,11 +12,41 @@ import UIKit
 class PhoneVerificationViewController: UIViewController {
     
     @IBOutlet weak var verifyButton: UIButton!
-    
+    @IBOutlet weak var phoneNumberInput: UITextField!
     
     @IBAction func clickedVerifyButton(sender: AnyObject) {
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setInteger(2, forKey: "verificationStatus") // set to verified
+        if (!phoneNumberInput.text.isEmpty) {
+            var phoneNumber = phoneNumberInput.text
+            phoneNumber = "".join(phoneNumber.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet))
+            
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setInteger(2, forKey: "verificationStatus") // set to verified
+            defaults.setObject(phoneNumber, forKey: "mainUserPhoneNumber")
+            
+            // set or update main user contact information. Create contact here or in RefreshNetwork method?
+        }
+        else {
+            // prompt user with alert to enter valid phone number
+            let alert = UIAlertView()
+            alert.title = "Invalid Phone Number"
+            alert.message = "Please Enter a valid phone number."
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+        }
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapRecognizer)
+        
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
     
 }
